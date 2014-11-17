ef6a169cdd3509b03f67bb20f86bf427c13d84d2
Nijo Joseph <nijojoseph@acube.co>
2014-11-17 19:49:07 +0530

    trip booking

--- application/views/user-pages/trip-booking.php
+++ application/views/user-pages/trip-booking.php
@@ -45,37 +45,38 @@ $drop_time='';
 					<table class="table-width-100-percent-td-width-25-percent">	
 						<tr>
 							<td>
-								<div class="form-group margin-10-px">
+								<div class="form-group margin-10-px margin-top-less-6">
 									<?php echo form_label('Name','namelabel'); ?>
 									<?php echo form_input(array('name'=>'name','class'=>'form-control','placeholder'=>'Enter Name','value'=>$name)); ?>
 									<?php echo $this->form_functions->form_error_session('name', '<p class="text-red">', '</p>'); ?>
 								</div>
 							</td>
 							<td>
-								<div class="form-group margin-10-px">
+								<div class="form-group margin-10-px margin-top-less-6">
 									<?php echo form_label('Mobile','mobilelabel'); ?>
 									<?php echo form_input(array('name'=>'mobile','class'=>'form-control','placeholder'=>'Enter Mobile','value'=>$mobile)); ?>
 									<?php echo $this->form_functions->form_error_session('mobile', '<p class="text-red">', '</p>'); ?>
 								</div>
 							</td>
 							<td>
-								<div class="form-group margin-10-px">
+								<div class="form-group margin-10-px margin-top-less-6">
 									<button class="btn btn-info btn-lg add-customer hide-me" type="button">ADD</button>
 									<button class="btn btn-danger btn-lg clear-customer" type="button">CLEAR</button>
 								</div>
 							</td>
+							
+							
+						</tr>
+						<tr>
 							<td>
-								<div class="form-group margin-10-px">
+								<div class="form-group margin-10-px margin-top-less-6">
 									<?php echo form_label('Pickup','pickuplabel'); ?>
 									<?php echo form_input(array('name'=>'trip_from','class'=>'form-control','placeholder'=>'Enter Pick Up','value'=>$trip_from)); ?>
 									<?php echo $this->form_functions->form_error_session('trip_from', '<p class="text-red">', '</p>'); ?>
 								</div>
 							</td>
-							
-						</tr>
-						<tr>
 							<td>
-								<div class="form-group margin-10-px">
+								<div class="form-group margin-10-px margin-top-less-6">
 									<?php echo form_label('Pickup Landmark','pickuplandmarklabel'); ?>
 									<?php echo form_input(array('name'=>'trip_from_landmark','class'=>'form-control','placeholder'=>'Enter Pick Up Landmark','value'=>$trip_from_landmark)); ?>
 									<?php echo $this->form_functions->form_error_session('trip_from_landmark', '<p class="text-red">', '</p>'); ?>
@@ -83,53 +84,54 @@ $drop_time='';
 							</td>
 							
 							<td>
-								<div class="form-group margin-10-px">
+								
+							</td>
+							
+							
+							
+					</tr>
+					<tr>
+							
+							<td>
+								<div class="form-group margin-10-px margin-top-less-6">
 									<?php echo form_label('Drop','droplabel'); ?>
 									<?php echo form_input(array('name'=>'trip_to','class'=>'form-control','placeholder'=>'Enter Drop','value'=>$trip_to)); ?>
 									<?php echo $this->form_functions->form_error_session('trip_to', '<p class="text-red">', '</p>'); ?>
 								</div>
 							</td>
 							<td>
-								<div class="form-group margin-10-px">
+								<div class="form-group margin-10-px margin-top-less-6">
 									<?php echo form_label('Drop Landmark','droplandmarklabel'); ?>
 									<?php echo form_input(array('name'=>'trip_to_landmark','class'=>'form-control','placeholder'=>'Enter Drop Landmark','value'=>$trip_to_landmark)); ?>
 									<?php echo $this->form_functions->form_error_session('trip_to_landmark', '<p class="text-red">', '</p>'); ?>
 								</div>
 							</td>
 							<td>
-								<div class="form-group margin-10-px">
-									<?php echo form_label('Pickup Date','pickupdatelabel'); ?>
-									<?php echo form_input(array('name'=>'pick_up_date','class'=>'form-control','placeholder'=>'Enter Pick Up Date','value'=>$pick_up_date)); ?>
-									<?php echo $this->form_functions->form_error_session('pick_up_date', '<p class="text-red">', '</p>'); ?>
-								</div>
+								<div class="input-group margin-10-px ">
+                                        <input class="form-control" type="text">
+                                        <span class="input-group-addon"><i class="fa fa-check"></i></span>
+                                    </div>
 							</td>
 							
 					</tr>
 					<tr>
 							
 							<td>
-								<div class="form-group margin-10-px">
+								<div class="form-group margin-10-px margin-top-less-6">
+									<?php echo form_label('Pickup Date','pickupdatelabel'); ?>
+									<?php echo form_input(array('name'=>'pick_up_date','class'=>'form-control','placeholder'=>'Enter Pick Up Date','value'=>$pick_up_date)); ?>
+									<?php echo $this->form_functions->form_error_session('pick_up_date', '<p class="text-red">', '</p>'); ?>
+								</div>
+							</td>
+							<td>
+								<div class="form-group margin-10-px margin-top-less-6">
 									<?php echo form_label('Pickup Time','pickuptimelabel'); ?>
 									<?php echo form_input(array('name'=>'pick_up_time','class'=>'form-control','placeholder'=>'Enter Pickup Time','value'=>$pick_up_time)); ?>
 									<?php echo $this->form_functions->form_error_session('pick_up_time', '<p class="text-red">', '</p>'); ?>
 								</div>
 							</td>
 							<td>
-								<div class="form-group margin-10-px">
-									<?php echo form_label('Drop Date','dropdatelabel'); ?>
-									<?php echo form_input(array('name'=>'drop_date','class'=>'form-control','placeholder'=>'Enter Pick Up Date','value'=>$drop_date)); ?>
-									<?php echo $this->form_functions->form_error_session('drop_date', '<p class="text-red">', '</p>'); ?>
-								</div>
-							</td>
-							<td>
-								<div class="form-group margin-10-px">
-									<?php echo form_label('Drop Time','pickuptimelabel'); ?>
-									<?php echo form_input(array('name'=>'drop_time','class'=>'form-control','placeholder'=>'Enter Drop Time','value'=>$drop_time)); ?>
-									<?php echo $this->form_functions->form_error_session('drop_time', '<p class="text-red">', '</p>'); ?>
-								</div>
-							</td>
-							<td>
-								<div class="form-group margin-10-px">
+								<div class="form-group margin-10-px margin-top-less-6">
 									<button class="btn btn-success btn-lg 	search-vehicles" type="button">SEARCH VEHICLES</button>
 									
 								</div>
