184774135bfb6864b3514a1f86f7888024ed5459
Nijo Joseph <nijojoseph@acube.co>
2014-12-02 11:32:13 +0530

    db data alterations

--- files/DB/safe_taxi_data.sql
+++ files/DB/safe_taxi_data.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 02, 2014 at 05:38 AM
+-- Generation Time: Dec 02, 2014 at 06:01 AM
 -- Server version: 5.5.37-0ubuntu0.13.10.1
 -- PHP Version: 5.5.3-1ubuntu2.6
 
@@ -13,6 +13,13 @@ SET time_zone = "+00:00";
 --
 -- Database: `safetaxi`
 --
+
+--
+-- Dumping data for table `ci_sessions`
+--
+
+INSERT INTO `ci_sessions` (`session_id`, `ip_address`, `user_agent`, `last_activity`, `user_data`) VALUES
+('eda9ebd180c12decb8e149142483453b', '127.0.0.1', 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:30.0) Gec', 1417499747, 'a:10:{s:9:"user_data";s:0:"";s:22:"isloginAttemptexceeded";b:0;s:2:"id";s:1:"2";s:4:"name";s:11:"Nijo Joseph";s:5:"email";s:19:"nijojoseph@acube.co";s:8:"username";s:10:"nijojoseph";s:4:"type";s:1:"2";s:10:"permission";s:1:"2";s:10:"isLoggedIn";b:1;s:10:"token_pass";s:32:"bf8191475f55068537a0dc716078dddb";}');
 
 --
 -- Dumping data for table `customer_statuses`
@@ -117,7 +124,7 @@ INSERT INTO `user_types` (`id`, `name`, `description`) VALUES
 --
 
 INSERT INTO `voucher_types` (`id`, `name`, `description`, `value`, `user_id`, `created`, `updated`) VALUES
-(4, 'Invoice', 'Invoice', 0, 2, '2014-12-02 05:10:38', '0000-00-00 00:00:00'),
-(5, 'Payment', 'Payment', 0, 2, '2014-12-02 05:10:38', '0000-00-00 00:00:00'),
-(6, 'Receipt', 'Receipt', 0, 2, '2014-12-02 05:11:18', '0000-00-00 00:00:00');
+(1, 'Invoice', 'Invoice', 0, 2, '2014-12-01 23:40:38', '0000-00-00 00:00:00'),
+(2, 'Payment', 'Payment', 0, 2, '2014-12-01 23:40:38', '0000-00-00 00:00:00'),
+(3, 'Receipt', 'Receipt', 0, 2, '2014-12-01 23:41:18', '0000-00-00 00:00:00');
 
