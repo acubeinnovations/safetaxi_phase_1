5e1fee4399929f34d8737fc72b0a4192689dfcd6
Arun <arunr@acube.co>
2014-12-04 18:49:39 +0530

    modified print

--- application/views/user-pages/print_driver_payments.php
+++ application/views/user-pages/print_driver_payments.php
@@ -1,92 +1,39 @@
-<?php 
-tcpdf();
-$pdf =  new TCPDF('P', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+<!-- CSS goes in the document HEAD or added to your external stylesheet -->
+<style type="text/css">
+table.gridtable {
+	font-family: verdana,arial,sans-serif;
+	font-size:11px;
+	color:#333333;
+	border-width: 1px;
+	border-color: #666666;
+	border-collapse: collapse;
+}
+table.gridtable th {
+	border-width: 1px;
+	padding: 8px;
+	border-style: solid;
+	border-color: #666666;
+	background-color: #dedede;
+}
+table.gridtable td {
+	border-width: 1px;
+	padding: 8px;
+	border-style: solid;
+	border-color: #666666;
+	background-color: #ffffff;
+}
+</style>
 
-// set document information
-$pdf->SetCreator(PDF_CREATOR);
-$pdf->SetAuthor('SafeTaxi');
-$pdf->SetTitle('Driver Payments');
-
-
-// set default header data
-$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE, PDF_HEADER_STRING);
-
-// set header and footer fonts
-$pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
-$pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
-
-// set default monospaced font
-$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
-
-// set margins
-$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
-$pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
-$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
-
-// set auto page breaks
-$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
-
-// set image scale factor
-$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
-
-
-
-// ---------------------------------------------------------
-
-// set font
-$pdf->SetFont('helvetica', 'B', 20);
-
-// add a page
-$pdf->AddPage();
-
-//$pdf->Write(0, 'Example of HTML tables', '', 0, 'L', true, 0, false, false, 0);
-
-$pdf->SetFont('helvetica', '', 8);
-
-ob_start();
-$content='<table border="1" style="line-height: 10px">
-
+<!-- Table goes in the document BODY -->
+<table class="gridtable" width="100%">
+<tr>
+	<th>Sl.no</th><th>Particulars</th><th>Amount</th>
+</tr>
 <tr>
 
-<td>Sl.No</td>
-<td>Date</td>
-<td>Vehicle Number</td>
-<td>Type</td>
-<td>Amount</td>
+	<td width="10%">1</td>
+	<td width="70%"></td>
+	<td width="20%"><?php echo $values[0]['Driver_name']; ?></td>
+</tr>
 
-</tr> 
-
-<tr>
-
-<td>1</td>
-<td>'.$values[0]['Payment_date'].'</td>
-<td>'.$values[0]['Driver_vehicle_registration'].'</td>
-<td>'.$values[0]['Voucher_type'].'</td>
-<td>'.$values[0]['Driver_debit'].'</td>
-
-</tr> 
-<hr>
-
-
-<table>
-
-<table border="1" style="line-height: 10px"><tr><td>1</td><td>1</td></tr></table>
-
-
-';
-
-
-
-ob_end_clean();
-
-//$pdf->writeHTML($content, true, false, false, false, '');
-//$pdf->writeHTML($content1, true, false, false, false, '');
-
-//echo $content; exit;
-$pdf->writeHTML($content, true, false, false, false, '');
-$file_name='Payment-'.$values[0]['Driver_name'].'.pdf';
-$pdf->Output($file_name, 'I');
-
-
-
-?>
+</table>
